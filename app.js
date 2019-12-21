ab13f3659089108871441753bb769e93d937a212
Sophie <sophie88leigh@gmail.com>
2019-12-21 10:40:45 -0500

    left join  employee view command line

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const viewOptions = [
     "View Departments",
     "View Roles",
     "View Employees",
+    "Add Employee",
     "exit"
 ]
 
@@ -30,12 +31,19 @@ function runSearch() {
                 case viewOptions[2]:
                     employeeView();
                     break;
+
                 case viewOptions[3]:
+                    addEmployee();
+                    break;
+
+                case viewOptions[4]:
                     connection.end();
                     break
             }
         })
 }
+
+
 
 function departmentView() {
     var sqlStr = "SELECT * FROM department";
@@ -47,7 +55,7 @@ function departmentView() {
     })
 }
 function employeeView() {
-    var sqlStr = "SELECT * FROM employee ";
+    var sqlStr = "SELECT first_name, last_name, title, salary FROM employee ";
     sqlStr += "LEFT JOIN role ";
     sqlStr += "ON employee.role_id = role.id"
     connection.query(sqlStr, function (err, result) {
@@ -68,76 +76,75 @@ function roleView() {
 }
 
 
-const addFlavor = () => {
-    const query = connection.query(
-        "INSERT INTO products SET ?",
-        {
-            flavor: "rocky road",
-            price: 3.99,
-            quantity: 135
-        },
-//put this stuff into our products table
-//then write call back function to tell us if there's error
-        (err, res) => {
-            if (err) {
-                throw err;
-            }
+// const addEmployee = () => {
+//     const query = connection.query(
+//         "INSERT INTO employee SET ?",
+//         {
+//             flavor: "rocky road",
+//             price: 3.99,
+//             quantity: 135
+//         },
+// //put this stuff into our products table
+// //then write call back function to tell us if there's error
+//         (err, res) => {
+//             if (err) {
+//                 throw err;
+//             }
            
-            console.table(res);
-        }
-    );
-    console.log(query.sql)
-    //tells what query it's running and sending to the sql server
-}
+//             console.table(res);
+//         }
+//     );
+//     console.log(query.sql)
+//     //tells what query it's running and sending to the sql server
+// }
 
-connection.connect((err) => {
-    if (err) {
-        throw err;
-    }
-    //everything we do has to be in the connection.connect()
-     // addFlavor()
-     addFlavor();
+// connection.connect((err) => {
+//     if (err) {
+//         throw err;
+//     }
+//     //everything we do has to be in the connection.connect()
+//      // addFlavor()
+//      addFlavor();
      
-});
+// });
 
 
-const updateFlavor = () => {
-    connection.query(
-        "UPDATE products SET ? WHERE ?",
-        [
-            //correspond to first question mark
-            {
-                quantity: 50
-            },
-            //correspond to second
-            {
-                flavor: "vanilla"
-            }
-        ],
-        (err, res)=> {
-            if (err) {
-                throw err;
-            }
-            //updateFlavor();
-            updateFlavor();
+// const updateFlavor = () => {
+//     connection.query(
+//         "UPDATE products SET ? WHERE ?",
+//         [
+//             //correspond to first question mark
+//             {
+//                 quantity: 50
+//             },
+//             //correspond to second
+//             {
+//                 flavor: "vanilla"
+//             }
+//         ],
+//         (err, res)=> {
+//             if (err) {
+//                 throw err;
+//             }
+//             //updateFlavor();
+//             updateFlavor();
 
-            console.log(res);
-            connection.end();
-        }
+//             console.log(res);
+//             connection.end();
+//         }
         
-    );
-}
-const deleteFlavor = (id) => {
-    connection.query(
-        "DELETE FROM products WHERE ?",
-        [
-            id: id
-        ],
-        (err, res)=> {
-            if (err) {
-                throw err;
-        }
-        deleteFlavor(5);
-        deleteFlavor(6);
-    )
-}
+//     );
+// }
+// const deleteFlavor = (id) => {
+//     connection.query(
+//         "DELETE FROM products WHERE id = ?",
+//         [
+//             id
+//         ],
+//         (err, res)=> {
+//             if (err) {
+//                 throw err;
+//             }
+//         }
+//     )
+// }
